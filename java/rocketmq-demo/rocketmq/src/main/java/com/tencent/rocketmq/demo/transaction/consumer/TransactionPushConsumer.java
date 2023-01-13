a2e2ff388511acfde2d419f2f45de587d7bd8f43
leizhiyuan <leizhiyuan@gmail.com>
2023-01-13 18:12:08 +0800

    feat: add transaction message

---
+++ java/rocketmq-demo/rocketmq/src/main/java/com/tencent/rocketmq/demo/transaction/consumer/TransactionPushConsumer.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.tencent.rocketmq.demo.transaction.consumer;
+
+import com.tencent.rocketmq.demo.common.ClientCreater;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+
+/**
+ * Description: 消息消费者(push)
+ */
+public class TransactionPushConsumer {
+
+
+    /**
+     * topic名称
+     */
+    private static final String TOPIC_NAME = "transaction_topic";
+
+    /**
+     * 消费者组名称
+     */
+    private static final String GROUP_NAME = "transaction_group";
+
+    public static void main(String[] args) throws Exception {
+        // 创建消息消费者
+        final DefaultMQPushConsumer pushConsumer = ClientCreater.createPushConsumer(GROUP_NAME);
+        // 订阅topic
+        pushConsumer.subscribe(TOPIC_NAME, "*");
+        // 注册回调实现类来处理从broker拉取回来的消息
+        pushConsumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            // 消息处理逻辑
+            System.out.printf("%s Receive transaction messages: %s %n", Thread.currentThread().getName(), msgs);
+            // 标记该消息已经被成功消费
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        // 启动消费者实例
+        pushConsumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
