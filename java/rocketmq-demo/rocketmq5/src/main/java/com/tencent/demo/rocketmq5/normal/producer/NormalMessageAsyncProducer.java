268431870618a7056053af3b1f29f3b93b4c1bd4
leizhiyuan <leizhiyuan@gmail.com>
2023-04-06 10:37:49 +0800

    feat: add rocketmq5 demo

---
+++ java/rocketmq-demo/rocketmq5/src/main/java/com/tencent/demo/rocketmq5/normal/producer/NormalMessageAsyncProducer.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.demo.rocketmq5.normal.producer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.client.java.example.AsyncProducerExample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NormalMessageAsyncProducer {
+    private static final Logger log = LoggerFactory.getLogger(AsyncProducerExample.class);
+
+    private NormalMessageAsyncProducer() {
+    }
+
+    public static void main(String[] args) throws ClientException, IOException, InterruptedException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+
+        // 添加配置的ak和sk
+        String accessKey = "yourAccessKey"; //ak
+        String secretKey = "yourSecretKey"; //sk
+        SessionCredentialsProvider sessionCredentialsProvider =
+            new StaticSessionCredentialsProvider(accessKey, secretKey);
+
+        // 填写腾讯云提供的接入地址
+        String endpoints = "foobar.com:8080";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setEndpoints(endpoints)
+            .enableSsl(false)
+            .setCredentialProvider(sessionCredentialsProvider)
+            .build();
+        String topic = "yourTopic";
+        // In most case, you don't need to create too many producers, singleton pattern is recommended.
+        final Producer producer = provider.newProducerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            // Set the topic name(s), which is optional but recommended. It makes producer could prefetch the topic
+            // route before message publishing.
+            .setTopics(topic)
+            // May throw {@link ClientException} if the producer is not initialized.
+            .build();
+        // Define your message body.
+        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
+        //不需要就不用设置了
+        String tag = "yourMessageTagA";
+        final Message message = provider.newMessageBuilder()
+            // Set topic for the current message.
+            .setTopic(topic)
+            // Message secondary classifier of message besides topic.
+            .setTag(tag)
+            // Key(s) of the message, another way to mark message besides message id.
+            .setKeys("yourMessageKey-0e094a5f9d85")
+            .setBody(body)
+            .build();
+        // Set individual thread pool for send callback.
+        final CompletableFuture<SendReceipt> future = producer.sendAsync(message);
+        ExecutorService sendCallbackExecutor = Executors.newCachedThreadPool();
+        future.whenCompleteAsync((sendReceipt, throwable) -> {
+            if (null != throwable) {
+                log.error("Failed to send message", throwable);
+                // Return early.
+                return;
+            }
+            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        }, sendCallbackExecutor);
+        // Block to avoid exist of background threads.
+        Thread.sleep(Long.MAX_VALUE);
+        // Close the producer when you don't need it anymore.
+        producer.close();
+    }
+}
---
+++ java/rocketmq-demo/rocketmq5/src/main/java/com/tencent/demo/rocketmq5/transaction/producer/TransactionMessageSyncProducer.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.demo.rocketmq5.transaction.producer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.apache.rocketmq.client.apis.producer.Transaction;
+import org.apache.rocketmq.client.apis.producer.TransactionChecker;
+import org.apache.rocketmq.client.apis.producer.TransactionResolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransactionMessageSyncProducer {
+    private static final Logger log = LoggerFactory.getLogger(TransactionMessageSyncProducer.class);
+
+    private TransactionMessageSyncProducer() {
+    }
+
+    public static void main(String[] args) throws ClientException, IOException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+
+        // 添加配置的ak和sk
+        String accessKey = "yourAccessKey"; //ak
+        String secretKey = "yourSecretKey"; //sk
+        SessionCredentialsProvider sessionCredentialsProvider =
+            new StaticSessionCredentialsProvider(accessKey, secretKey);
+        // 填写腾讯云提供的接入地址
+        String endpoints = "foobar.com:8080";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setEndpoints(endpoints)
+            .enableSsl(false)
+            .setCredentialProvider(sessionCredentialsProvider)
+            .build();
+        String topic = "yourTransactionTopic";
+
+        //用来回查的
+        TransactionChecker checker = messageView -> {
+            log.info("Receive transactional message check, message={}", messageView);
+            // Return the transaction resolution according to your business logic.
+            return TransactionResolution.COMMIT;
+        };
+        // In most case, you don't need to create too many producers, singleton pattern is recommended.
+        Producer producer = provider.newProducerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            // Set the topic name(s), which is optional but recommended. It makes producer could prefetch the topic
+            // route before message publishing.
+            .setTopics(topic)
+            // 需要设置事务检查器
+            .setTransactionChecker(checker)
+            .build();
+        final Transaction transaction = producer.beginTransaction();
+        // Define your message body.
+        byte[] body = "This is a transaction message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
+        String tag = "yourMessageTagA";
+        final Message message = provider.newMessageBuilder()
+            // Set topic for the current message.
+            .setTopic(topic)
+            // Message secondary classifier of message besides topic.
+            .setTag(tag)
+            // Key(s) of the message, another way to mark message besides message id.
+            .setKeys("yourMessageKey-565ef26f5727")
+            .setBody(body)
+            .build();
+        try {
+            final SendReceipt sendReceipt = producer.send(message, transaction);
+            log.info("Send transaction message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (Throwable t) {
+            log.error("Failed to send message", t);
+            return;
+        }
+
+        //当用户业务处理成功后，提交事务
+        // Commit the transaction.
+        transaction.commit();
+        // Or rollback the transaction.
+
+        //如果失败了，那么就手动回滚掉
+        // transaction.rollback();
+        // Close the producer when you don't need it anymore.
+        producer.close();
+    }
+}
