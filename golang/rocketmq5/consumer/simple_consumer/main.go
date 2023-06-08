53037ffb9a11f965e16cdea6276dbdc73a0bac32
htaowang <htaowang@tencent.com>
2023-06-08 17:34:12 +0800

    add golang rocketmq5 demo

---
+++ golang/rocketmq5/consumer/simple_consumer/main.go
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	rmq_client "github.com/apache/rocketmq-clients/golang/v5"
+	"github.com/apache/rocketmq-clients/golang/v5/credentials"
+)
+
+const (
+	Topic         = "xxxxxx"
+	ConsumerGroup = "xxxxxx"
+	// Endpoint 填写腾讯云提供的接入地址
+	Endpoint = "xxxxxx"
+	// AccessKey 添加配置的ak
+	AccessKey = "xxxxxx"
+	// SecretKey 添加配置的sk
+	SecretKey = "xxxxxx"
+)
+
+var (
+	// maximum waiting time for receive func
+	awaitDuration = time.Second * 5
+	// maximum number of messages received at one time
+	maxMessageNum int32 = 16
+	// invisibleDuration should > 20s
+	invisibleDuration = time.Second * 20
+	// receive messages in a loop
+)
+
+func main() {
+	// log to console
+	os.Setenv("mq.consoleAppender.enabled", "true")
+	rmq_client.ResetLogger()
+	// In most case, you don't need to create many consumers, singleton pattern is more recommended.
+	simpleConsumer, err := rmq_client.NewSimpleConsumer(&rmq_client.Config{
+		Endpoint:      Endpoint,
+		ConsumerGroup: ConsumerGroup,
+		Credentials: &credentials.SessionCredentials{
+			AccessKey:    AccessKey,
+			AccessSecret: SecretKey,
+		},
+	},
+		rmq_client.WithAwaitDuration(awaitDuration),
+		rmq_client.WithSubscriptionExpressions(map[string]*rmq_client.FilterExpression{
+			Topic: rmq_client.SUB_ALL,
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// start simpleConsumer
+	err = simpleConsumer.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// graceful stop simpleConsumer
+	defer simpleConsumer.GracefulStop()
+	for {
+		fmt.Println("start receive message")
+		mvs, err := simpleConsumer.Receive(context.TODO(), maxMessageNum, invisibleDuration)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// ack message
+		for _, mv := range mvs {
+			simpleConsumer.Ack(context.TODO(), mv)
+			fmt.Println(mv)
+		}
+		fmt.Println("wait a moment")
+		fmt.Println()
+		time.Sleep(time.Second * 1)
+	}
+}
---
+++ golang/rocketmq5/producer/transaction/main.go
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	rmq_client "github.com/apache/rocketmq-clients/golang/v5"
+	"github.com/apache/rocketmq-clients/golang/v5/credentials"
+)
+
+const (
+	Topic = "xxxxxx"
+	// Endpoint 填写腾讯云提供的接入地址
+	Endpoint = "xxxxxx"
+	// AccessKey 添加配置的ak
+	AccessKey = "xxxxxx"
+	// SecretKey 添加配置的sk
+	SecretKey = "xxxxxx"
+)
+
+func main() {
+	// log to console
+	os.Setenv("mq.consoleAppender.enabled", "true")
+	rmq_client.ResetLogger()
+	// In most case, you don't need to create many producers, singleton pattern is more recommended.
+	producer, err := rmq_client.NewProducer(&rmq_client.Config{
+		Endpoint: Endpoint,
+		Credentials: &credentials.SessionCredentials{
+			AccessKey:    AccessKey,
+			AccessSecret: SecretKey,
+		},
+	},
+		rmq_client.WithTransactionChecker(&rmq_client.TransactionChecker{
+			Check: func(msg *rmq_client.MessageView) rmq_client.TransactionResolution {
+				log.Printf("check transaction message: %v", msg)
+				return rmq_client.COMMIT
+			},
+		}),
+		rmq_client.WithTopics(Topic),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// start producer
+	err = producer.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// graceful stop producer
+	defer producer.GracefulStop()
+	for i := 0; i < 10; i++ {
+		// new a message
+		msg := &rmq_client.Message{
+			Topic: Topic,
+			Body:  []byte("this is a message : " + strconv.Itoa(i)),
+		}
+		// set keys and tag
+		msg.SetKeys("a", "b")
+		msg.SetTag("ab")
+		// send message in sync
+		transaction := producer.BeginTransaction()
+		resp, err := producer.SendWithTransaction(context.TODO(), msg, transaction)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < len(resp); i++ {
+			fmt.Printf("%#v\n", resp[i])
+		}
+		// commit transaction message
+		err = transaction.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// wait a moment
+		time.Sleep(time.Second * 1)
+	}
+}
