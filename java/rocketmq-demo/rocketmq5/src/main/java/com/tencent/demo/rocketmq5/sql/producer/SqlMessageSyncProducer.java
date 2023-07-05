9ed1f64e53ef11cdd63ba1460d74a7f1aecd15ce
leizhiyuan <leizhiyuan@gmail.com>
2023-07-05 14:03:24 +0800

    feat: add tag and sql

---
+++ java/rocketmq-demo/rocketmq5/src/main/java/com/tencent/demo/rocketmq5/sql/producer/SqlMessageSyncProducer.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.demo.rocketmq5.sql.producer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.rocketmq.client.apis.ClientConfiguration;
+import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.rocketmq.client.apis.ClientServiceProvider;
+import org.apache.rocketmq.client.apis.SessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.StaticSessionCredentialsProvider;
+import org.apache.rocketmq.client.apis.message.Message;
+import org.apache.rocketmq.client.apis.producer.Producer;
+import org.apache.rocketmq.client.apis.producer.SendReceipt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SqlMessageSyncProducer {
+    private static final Logger log = LoggerFactory.getLogger(SqlMessageSyncProducer.class);
+
+    private SqlMessageSyncProducer() {
+    }
+
+    public static void main(String[] args) throws ClientException, IOException {
+        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+
+        // 添加配置的ak和sk
+        String accessKey = "yourAccessKey"; //ak
+        String secretKey = "yourSecretKey"; //sk
+        SessionCredentialsProvider sessionCredentialsProvider =
+            new StaticSessionCredentialsProvider(accessKey, secretKey);
+
+        // 填写腾讯云提供的接入地址
+        String endpoints = "rmq-xxx.rocketmq.xxxtencenttdmq.com:8080";
+        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
+            .setEndpoints(endpoints)
+            .enableSsl(false)
+            .setCredentialProvider(sessionCredentialsProvider)
+            .build();
+        String topic = "yourNormalTopic";
+        // In most case, you don't need to create too many producers, singleton pattern is recommended.
+        final Producer producer = provider.newProducerBuilder()
+            .setClientConfiguration(clientConfiguration)
+            // Set the topic name(s), which is optional but recommended. It makes producer could prefetch the topic
+            // route before message publishing.
+            .setTopics(topic)
+            // May throw {@link ClientException} if the producer is not initialized.
+            .build();
+        // Define your message body.
+        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
+        final Message message = provider.newMessageBuilder()
+            // Set topic for the current message.
+            .setTopic(topic)
+            // Message secondary classifier of message besides topic.
+            // Key(s) of the message, another way to mark message besides message id.
+            .setKeys("yourMessageKey-1c151062f96e")
+            .setBody(body)
+            //一些用于sql过滤的信息
+            .addProperty("key1", "value1")
+            .build();
+        try {
+            final SendReceipt sendReceipt = producer.send(message);
+            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+        } catch (Throwable t) {
+            log.error("Failed to send message", t);
+        }
+        // Close the producer when you don't need it anymore.
+        producer.close();
+    }
+}
