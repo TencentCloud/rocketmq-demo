9cfa41850a1188ce25bb8748a143cba0941dd629
qipingluo <qipingluo@tencent.com>
2024-02-18 16:31:53 +0800

    [rust]add delay producer example

---
+++ rust/rocketmq5/src/bin/delay_producer.rs
@@ -0,0 +1,45 @@
+use std::{
+    ops::Add,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
+
+use rocketmq::model::message::MessageBuilder;
+
+mod util;
+
+#[tokio::main]
+async fn main() {
+    let topic = "test-delay";
+    let mut producer = util::new_producer(topic).unwrap();
+    let start_result = producer.start().await;
+    if start_result.is_err() {
+        eprintln!("producer start failure: {:?}", start_result.unwrap_err());
+        return;
+    }
+
+    let message = MessageBuilder::delay_message_builder(
+        topic,
+        "hello delay message".as_bytes().to_vec(),
+        SystemTime::now()
+            .add(Duration::from_secs(600))
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as i64,
+    )
+    .set_tag("tagA")
+    .set_keys(vec!["keyA"])
+    .build()
+    .unwrap();
+
+    let send_result = producer.send(message).await;
+    if send_result.is_err() {
+        eprintln!("send message error: {:?}", send_result.unwrap_err());
+        return;
+    }
+    println!(
+        "send message success, id = {}",
+        send_result.unwrap().message_id()
+    );
+
+    _ = producer.shutdown().await;
+}
