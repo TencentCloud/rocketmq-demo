802bc8a15bf9f47fa1edd5a60d5a52853eb57f32
leizhiyuan <leizhiyuan@gmail.com>
2025-06-12 10:39:26 +0800

    feat: add C# demo for RocketMQ push consumer

---
+++ csharp/rocketmq5/rocketmq5_csharp_demo/PushConsumerDemo.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Org.Apache.Rocketmq;
+
+
+namespace examples
+{
+    public class PushConsumerExample
+    {
+        private static readonly ILogger Logger = MqLogManager.CreateLogger(typeof(PushConsumerExample).FullName);
+
+
+        private static readonly string Endpoint = Environment.GetEnvironmentVariable("ROCKETMQ_ENDPOINT");
+
+
+        internal static async Task QuickStart()
+        {
+            const string accessKey = "yourAccessKey";
+            const string secretKey = "yourSecretKey";
+            // Enable the switch if you use .NET Core 3.1 and want to disable TLS/SSL.
+            // AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+
+            // Credential provider is optional for client configuration.
+            var credentialsProvider = new StaticSessionCredentialsProvider(accessKey, secretKey);
+            const string endpoints = "rmq-xxx.rocketmq.gz.qcloud.tencenttdmq.com:8080"; // 腾讯云接入点
+            var clientConfig = new ClientConfig.Builder()
+                .SetEndpoints(Endpoint)
+                .SetCredentialsProvider(credentialsProvider)
+                .Build();
+
+
+            // Add your subscriptions.
+            const string consumerGroup = "yourConsumerGroup";
+            const string topic = "yourTopic";
+            var subscription = new Dictionary<string, FilterExpression>
+                { { topic, new FilterExpression("*") } };
+
+
+            var pushConsumer = await new PushConsumer.Builder()
+                .SetClientConfig(clientConfig)
+                .SetConsumerGroup(consumerGroup)
+                .SetSubscriptionExpression(subscription)
+                .SetMessageListener(new CustomMessageListener())
+                .Build();
+
+
+            Thread.Sleep(Timeout.Infinite);
+
+
+            // Close the push consumer if you don't need it anymore.
+            // await pushConsumer.DisposeAsync();
+        }
+
+
+        private class CustomMessageListener : IMessageListener
+        {
+            public ConsumeResult Consume(MessageView messageView)
+            {
+                // Handle the received message and return consume result.
+                Logger.LogInformation($"Consume message={messageView}");
+                return ConsumeResult.SUCCESS;
+            }
+        }
+    }
+}
